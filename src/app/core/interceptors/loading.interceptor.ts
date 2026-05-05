4f74437e4ca0ac71360486d46f64cb7a80da63cc
PnhhUser <pnhh240300@gmail.com>
2026-05-06 06:32:55 +0700

    remove delay

--- src/app/core/interceptors/loading.interceptor.ts
+++ src/app/core/interceptors/loading.interceptor.ts
@@ -1,15 +1,12 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { LoaderService } from '../services/loader.service';
-import { delay, finalize } from 'rxjs';
+import { finalize } from 'rxjs';
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   const loaderService = inject(LoaderService);
 
   loaderService.turnOn();
 
-  return next(req).pipe(
-    delay(1000),
-    finalize(() => loaderService.turnOff()),
-  );
+  return next(req).pipe(finalize(() => loaderService.turnOff()));
 };
