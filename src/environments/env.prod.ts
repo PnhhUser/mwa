1f34f5c8e8901776b6f20f3bcaaa633ad109e923
PnhhUser <pnhh240300@gmail.com>
2026-05-08 15:07:19 +0700

    Update env

--- src/environments/env.prod.ts
+++ src/environments/env.prod.ts
@@ -1,6 +1,9 @@
 const env = {
   production: true,
-  apiUrl: 'https://webappbe-production.up.railway.app/api',
+  apiUrl: 'https://webappbe-nrox.onrender.com/api',
 };
+
+// https://webappbe-nrox.onrender.com/api
+// https://webappbe-production.up.railway.app/api
 
 export default env;
